12187951ea9ea109a6855aa7b8e2a036d7e4fb7b
goobergaze <gregmaxsold@gmail.com>
2016-04-26 18:39:49 +0200

    Added files via upload

---
+++ home_made/master_a.ino
@@ -0,0 +1,133 @@
+// REMOTE CONTROL CENTRAL CONNECTION
+// Bluetooth unit: CZ-HC-05 (HC-05 clone)
+// I/O: IR remote and 6-LED indicator
+
+#include <SoftwareSerial.h>
+#include <PLab_IRremote.h>
+
+#define PIN_LED_400  2
+#define PIN_LED_200  3
+#define PIN_LED_100  4
+#define PIN_LED_50   5
+#define PIN_LED_25   6
+#define PIN_LED_10   7
+#define PIN_IR_RECV  8
+#define PIN_BLT_PWR  A0
+#define PIN_BLT_TXD  A1
+#define PIN_BLT_RXD  A2
+#define PIN_BLT_KEY  A3
+
+SoftwareSerial bluetooth(PIN_BLT_TXD, PIN_BLT_RXD);
+IRrecv irRecv(PIN_IR_RECV);
+decode_results irResults;
+
+char commBuf[80] = {0};
+char* lastCommand = "SMove:N";
+
+void setup(){
+  pinMode(PIN_LED_400 , OUTPUT);
+  pinMode(PIN_LED_200 , OUTPUT);
+  pinMode(PIN_LED_100 , OUTPUT);
+  pinMode(PIN_LED_50  , OUTPUT);
+  pinMode(PIN_LED_25  , OUTPUT);
+  pinMode(PIN_LED_10  , OUTPUT);
+  pinMode(PIN_BLT_KEY , OUTPUT);
+  pinMode(PIN_BLT_PWR , OUTPUT);
+
+  //Initialize Bluetooth unit in command mode
+  digitalWrite(PIN_BLT_KEY, HIGH);
+  digitalWrite(PIN_BLT_PWR, HIGH);
+  bluetooth.begin(38400);
+
+  //Quick configuration
+  runCommand("AT+ORGL\r\n", true);
+  runCommand("AT+RMAAD\r\n", true);
+  runCommand("AT+PSWD=573000\r\n", true);
+  runCommand("AT+ROLE=1\r\n", true);
+  runCommand("AT+CMODE=1\r\n", true);
+  runCommand("AT+RESET\r\n", true);
+  runCommand("AT+INIT\r\n", true);
+
+  do{ //Try pairing with robot (fixed address)
+    runCommand("AT+PAIR=74DA,EA,AF83DB,20\r\n", false);
+  } while(strcmp(commBuf, "OK"));
+
+  //Pairing successful, bind address and reboot
+  runCommand("AT+CMODE=0\r\n", true);
+  runCommand("AT+BIND=74DA,EA,AF83DB\r\n", true);
+  digitalWrite(PIN_BLT_PWR, LOW);
+  digitalWrite(PIN_BLT_KEY, LOW);
+  delay(1000);
+  digitalWrite(PIN_BLT_PWR, HIGH);
+  bluetooth.begin(38400);
+
+  irRecv.enableIRIn();
+}
+void loop(){
+  //IR input lag workaround
+  unsigned long timeout = millis();
+  bool cmdModified = false;
+  while(millis() - timeout < 150)
+    if(irRecv.decode(&irResults)){
+      cmdModified = true;
+      irRecv.resume();
+      break;
+    }
+
+  //Modify command based on button press, or lack thereof
+  if(!cmdModified)  lastCommand[6] = 'N';
+  else switch(irResults.value){
+    case IR_UP:     lastCommand[6] = 'U'; break;
+    case IR_LEFT:   lastCommand[6] = 'L'; break;
+    case IR_DOWN:   lastCommand[6] = 'D'; break;
+    case IR_RIGHT:  lastCommand[6] = 'R'; break;
+    case IR_REPEAT: break;
+    default:        lastCommand[6] = 'N';
+  }
+  runCommand(lastCommand, true);
+
+  //Once command is sent, response will land in char* commBuf
+  float sonarDist = atof(commBuf);
+
+  //Indicate result with a "distance bar" of consecutive LEDs
+  digitalWrite(PIN_LED_400 , sonarDist && sonarDist <= 400);
+  digitalWrite(PIN_LED_200 , sonarDist && sonarDist <= 200);
+  digitalWrite(PIN_LED_100 , sonarDist && sonarDist <= 100);
+  digitalWrite(PIN_LED_50  , sonarDist && sonarDist <= 50);
+  digitalWrite(PIN_LED_25  , sonarDist && sonarDist <= 25);
+  digitalWrite(PIN_LED_10  , sonarDist && sonarDist <= 10);
+}
+
+/* ---------------------- */
+void runCommand(char* cmd, bool enableResend){
+  //First transmit
+  bluetooth.write(cmd);
+
+  if(enableResend){ //Wait for response, resend on timeout
+    unsigned long timestamp = millis();
+    while(!bluetooth.available())
+      if(millis() - timestamp >= 1000){
+        bluetooth.write(cmd);
+        timestamp = millis();
+      }
+  }
+  else //Wait patiently
+    while(!bluetooth.available());
+
+  //Message is underway, store it in char* commBuf
+  for(int index = 0; index < 79; index++){
+    //Abort if no null-terminator in sight
+    for(int abort = 0; !bluetooth.available(); abort++)
+      if(abort >= 250){
+        commBuf[index] = 0;
+        return;
+      }
+
+    //Store characters until NUL is read
+    commBuf[index] = bluetooth.read();
+    if(!commBuf[index])
+      break;
+  }
+  //Just to be sure...
+  commBuf[79] = 0;
+}
---
+++ home_made/slave_a.ino
@@ -0,0 +1,89 @@
+// ZUMO ROBOT PERIPHERAL CONNECTION
+// Bluetooth unit: BLE-CC41-A (HM-10 clone)
+// I/O: Ultrasound proximity sensor
+
+#include <SoftwareSerial.h>
+#include <ZumoMotors.h>
+#include <NewPing.h>
+
+SoftwareSerial bluetooth(A4, A5); //TX, RX
+ZumoMotors motors;
+NewPing sonar(2, 3, 400); //Trig, Echo, Max distance cm
+
+char commBuf[80] = {0};
+
+void setup(){
+  bluetooth.begin(9600);
+
+  //Quick configuration
+  runCommand("AT+RENEW");
+  runCommand("AT+NAMESemprini");
+  runCommand("AT+PASS573000");
+}
+void loop(){
+  //Listen for messages from master
+  while(!bluetooth.available());
+
+  //Copy message to char* commBuf
+  getResponse();
+
+  //Check message format, disconnect if invalid
+  char header[7] = {0};
+  memcpy(header, commBuf, 6);
+  bool invalidFormat = false;
+
+  if(strcmp(header, "SMove:"))
+    invalidFormat = true;
+  else switch(commBuf[6]){
+    case 'N': motors.setSpeeds(   0,    0); break; //stand still
+    case 'U': motors.setSpeeds( 200,  200); break; //forward
+    case 'L': motors.setSpeeds(-200,  200); break; //turn left
+    case 'D': motors.setSpeeds(-200, -200); break; //reverse
+    case 'R': motors.setSpeeds( 200, -200); break; //turn right
+    default:  invalidFormat = true;
+  }
+
+  if(invalidFormat){
+    motors.setSpeeds(0, 0);
+    runCommand("AT");
+  }
+  else{ //Provide response
+    sprintf(commBuf, "%f\r\n", sonar.convert_cm(sonar.ping()));
+    bluetooth.write(commBuf);
+  }
+}
+
+/* ---------------------- */
+void runCommand(char* cmd){
+  //First transmit
+  bluetooth.write(cmd);
+
+  //Wait for response, resend on timeout
+  unsigned long timestamp = millis();
+  while(!bluetooth.available())
+    if(millis() - timestamp >= 1000){
+      bluetooth.write(cmd);
+      timestamp = millis();
+    }
+
+  //Message is underway
+  getResponse();
+}
+void getResponse(){
+  //Saves response in external char* commBuf
+  for(int index = 0; index < 79; index++){
+    //Abort if no null-terminator in sight
+    for(int abort = 0; !bluetooth.available(); abort++)
+      if(abort >= 250){
+        commBuf[index] = 0;
+        return;
+      }
+
+    //Store characters until NUL is read
+    commBuf[index] = bluetooth.read();
+    if(!commBuf[index])
+      break;
+  }
+  //Just to be sure...
+  commBuf[79] = 0;
+}
